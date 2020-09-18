1115868b20fa44789b397ba02757b6045da3cf9a
Rohit Malhotra <rohitmalhotra@urbanclap.com>
2020-09-18 13:46:28 +0530

    basic setup uc lens

---
+++ app/src/main/java/com/example/uclens/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.example.uclens
+
+import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.os.Handler
+import android.view.MotionEvent
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+class SplashActivity : AppCompatActivity() {
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
