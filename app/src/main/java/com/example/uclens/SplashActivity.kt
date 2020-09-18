0fee88a1375ad5949d8cee92849e02b0bd0e81db
Devansh Bhatia <devanshbhatia@urbanclap.com>
2020-09-18 16:52:34 +0530

    Merging Tensorflow Lite

--- app/src/main/java/com/example/uclens/SplashActivity.kt
+++ app/src/main/java/com/example/uclens/SplashActivity.kt
@@ -2,6 +2,8 @@ package com.example.uclens
 
 import androidx.appcompat.app.AppCompatActivity
 import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.view.MotionEvent
@@ -9,6 +11,7 @@ import android.view.View
 import android.view.Window
 import android.view.WindowManager
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 
@@ -16,10 +19,15 @@ import android.widget.TextView
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : Activity() {
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        val detectorView: ImageView = findViewById(R.id.image_camera);
+        detectorView?.setOnClickListener() {
+            val intent = Intent(this@SplashActivity, DetectorActivity::class.java);
+            startActivity(intent);
+        }
     }
 }
